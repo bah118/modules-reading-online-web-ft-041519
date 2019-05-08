ee1e936d5bf4fb353aa1d193565888fe22b08c26
BethAnn Hanlon <herbivore137@gmail.com>
2019-05-08 03:11:57 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -3,6 +3,15 @@ require_relative './dance_module.rb'
 require_relative './fancy_dance.rb'
 
 class Dancer
+  extend MetaDancing
+  include Dance
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
+  
+  
+  attr_accessor :name
+ 
+  def initialize(name)
+    @name = name
+  end
 end
