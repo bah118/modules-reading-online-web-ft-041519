a23da270a9408f35fedf2f4cce0674e7db46b3f6
BethAnn Hanlon <herbivore137@gmail.com>
2019-05-08 03:10:03 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -2,10 +2,11 @@ require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
 class Dancer
+  extend MetaDancing
   
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
-  extend MetaDancing
+  
   
   attr_accessor :name
  
